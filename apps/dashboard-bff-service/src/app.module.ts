697690b1c18758e087968695f453d76a8a867705
Bohdan Shkaran <bogdan.shkaran@gmail.com>
2026-02-10 12:16:19 +0200

    feat:common: added health check

--- apps/dashboard-bff-service/src/app.module.ts
+++ apps/dashboard-bff-service/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from 'nestjs-env-getter';
 import { CacheModule } from '@common/cache';
+import { HealthModule } from '@common/health';
 
 import { AppConfig } from './app.config';
 import { AnalyticsModule } from './analytics/analytics.module';
@@ -8,6 +9,12 @@ import { AnalyticsModule } from './analytics/analytics.module';
 @Module({
   imports: [
     AppConfigModule.forRoot({ useClass: AppConfig }),
+    HealthModule.forRootAsync({
+      useFactory: ({ serviceName }: AppConfig) => ({
+        name: serviceName,
+      }),
+      inject: [AppConfig],
+    }),
     CacheModule.forRootAsync({
       useFactory: ({ stdTTL }: AppConfig) => ({ stdTTL }),
       inject: [AppConfig],
