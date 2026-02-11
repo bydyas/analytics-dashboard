76e92235b72cc77cf6c13cd70da1cf9aa9565bfa
Bohdan Shkaran <bogdan.shkaran@gmail.com>
2026-02-11 16:24:16 +0200

    fix:bff: added mocked provider into tests

--- apps/dashboard-bff-service/src/analytics/tests/analytics.service.spec.ts
+++ apps/dashboard-bff-service/src/analytics/tests/analytics.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { describe, it, beforeEach, expect, jest } from '@jest/globals';
 import { AnalyticsService } from '../analytics.service';
 import { CacheService } from '@common/cache';
+import { MockedLoggerServiceProvider } from '@common/logger';
 
 describe('AnalyticsService', () => {
   let service: AnalyticsService;
@@ -10,6 +11,7 @@ describe('AnalyticsService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         AnalyticsService,
+        MockedLoggerServiceProvider,
         {
           provide: CacheService,
           useValue: {
